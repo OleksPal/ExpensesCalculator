3cb091874912b5a26add18f01f59d66a75cb91ea
User <oleksandr.palamarchuk1310@gmail.com>
2024-05-05 14:18:41 +0300

    Replace the item list with a view component

---
+++ ViewComponents/ItemsListViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExpensesCalculator.Models;
+using ExpensesCalculator.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ExpensesCalculator.ViewComponents
+{
+    public class ItemsListViewComponent : ViewComponent
+    {
+        private readonly ExpensesContext _context;
+
+        public ItemsListViewComponent(ExpensesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int checkId)
+        {
+            var check = await _context.Checks.Include(c => c.Items).FirstOrDefaultAsync(c => c.Id == checkId);
+
+            if (check is null)
+            {
+                return (IViewComponentResult)Results.NoContent();
+            }
+
+            return View(check.Items);
+        }
+    }
+}
