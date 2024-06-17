0fe1f11a04449f9a11d150ba4cefc56e52fb28e7
User <oleksandr.palamarchuk1310@gmail.com>
2024-06-17 11:24:40 +0300

    Add the PeopleWithAccess column to the DayExpenses model and remove the VerificationPath column from the Check model

--- Models/Check.cs
+++ Models/Check.cs
@@ -5,7 +5,6 @@
         public List<Item> Items { get; set; }
         public double Sum { get; set; }
         public string Location { get; set; }
-        public string? VerificationPath { get; set; }
         public string Payer { get; set; }
     }
 }
