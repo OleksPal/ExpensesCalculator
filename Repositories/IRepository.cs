0ab6163d9bb68137786986d4eb3cde41d5dcbea3
User <oleksandr.palamarchuk1310@gmail.com>
2024-06-19 22:11:13 +0300

    Add the repositoty interface

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExpensesCalculator.Models;
+
+namespace ExpensesCalculator.Repositories
+{
+    public interface IRepository
+    {
+        public IEnumerable<DbObject> GetAll();
+        public DbObject GetById(int id);
+        public void Insert(DbObject obj);
+        public void Update(DbObject obj);
+        public void Delete(int id);
+    }
+}
