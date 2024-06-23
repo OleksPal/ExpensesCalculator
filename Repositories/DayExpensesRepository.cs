6b4be122701b99556b22cde4a0ea972eb9dc858c
User <oleksandr.palamarchuk1310@gmail.com>
2024-06-23 10:22:47 +0300

    Add a repository for the DayExpenses model

---
+++ Repositories/DayExpensesRepository.cs
@@ -0,0 +1,55 @@
+﻿using ExpensesCalculator.Data;
+using ExpensesCalculator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesCalculator.Repositories
+{
+    public class DayExpensesRepository : IDayExpensesRepository
+    {
+        private readonly ExpensesContext _context;
+        private readonly string _requestorName;
+
+        public DayExpensesRepository(ExpensesContext context, string requestorName)
+        {
+            _context = context;
+            _requestorName = requestorName;
+        }
+
+        public async Task<IEnumerable<DayExpenses>> GetAll()
+        {
+            return await _context.Days.Where(d => d.PeopleWithAccess.Contains(_requestorName)).ToListAsync();
+        }
+
+        public async Task<DayExpenses> GetById(int id)
+        {
+            return await _context.Days.Where(d => d.PeopleWithAccess.Contains(_requestorName))
+                .FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task Insert(DayExpenses dayExpenses)
+        {
+            await _context.Days.AddAsync(dayExpenses);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(DayExpenses dayExpenses)
+        {
+            if (dayExpenses.PeopleWithAccess.Contains(_requestorName))
+            {
+                _context.Days.Update(dayExpenses);
+                await _context.SaveChangesAsync();
+            }            
+        }
+
+        public async Task Delete(int id)
+        {
+            var dayToDelete = await _context.Days.FindAsync(id);
+
+            if (dayToDelete is not null && dayToDelete.PeopleWithAccess.Contains(_requestorName)) 
+            {
+                _context.Days.Remove(dayToDelete);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Repositories/IDayExpensesRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExpensesCalculator.Models;
+
+namespace ExpensesCalculator.Repositories
+{
+    public interface IDayExpensesRepository
+    {
+        Task<IEnumerable<DayExpenses>> GetAll();
+        Task<DayExpenses> GetById(int id);
+        Task Insert(DayExpenses dayExpenses);
+        Task Update(DayExpenses dayExpenses);
+        Task Delete(int id);
+    }
+}
