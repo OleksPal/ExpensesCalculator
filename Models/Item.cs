c2257f701529c7a13f076a6b548c94d7e904fec6
User <oleksandr.palamarchuk1310@gmail.com>
2024-06-24 21:57:39 +0300

    Add data annotations for the Item model

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,11 +1,28 @@
-﻿namespace ExpensesCalculator.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesCalculator.Models
 {
     public class Item : DbObject
     {
+        [Required(ErrorMessage = "Please enter item name")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
+        [StringLength(30, ErrorMessage = "Name can have a max of 30 characters")]
         public string Name { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Description")]
+        [StringLength(100, ErrorMessage = "Description can have a max of 100 characters")]
         public string? Description { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price")]
+        [DisplayFormat(DataFormatString = "{0}₴")]
+        [Range(0, Double.MaxValue, ErrorMessage = "Please enter correct price")]
         public decimal Price { get; set; }
+
         public ICollection<string> Users { get; } = new List<string>();
+
         public int CheckId { get; set; }
     }
 }
