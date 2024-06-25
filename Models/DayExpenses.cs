129271df281c9e465c64e45f38c6ba49f7cf9112
User <oleksandr.palamarchuk1310@gmail.com>
2024-06-25 11:07:33 +0300

    Add data annotations for the DayExpenses model

--- Models/DayExpenses.cs
+++ Models/DayExpenses.cs
@@ -1,10 +1,20 @@
-﻿namespace ExpensesCalculator.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesCalculator.Models
 {
     public class DayExpenses : DbObject
     {
         public ICollection<Check> Checks { get; } = new List<Check>();
+
+        [Required(ErrorMessage = "Please enter expenses date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
         public DateOnly Date { get; set; }
+
+        [Display(Name = "Participants")]
         public ICollection<string> Participants { get; } = new List<string>();
+
+        [Display(Name = "People with access")]
         public ICollection<string> PeopleWithAccess { get; } = new List<string>();
     }
 }
