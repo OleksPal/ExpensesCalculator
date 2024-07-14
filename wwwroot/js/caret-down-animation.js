52142ef4f9809ce68911ef9977085b57aa5d2c72
User <oleksandr.palamarchuk1310@gmail.com>
2024-07-14 09:48:45 +0300

    Fix caret down animation and refactor ExpensesCalculation view

---
+++ wwwroot/js/caret-down-animation.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("*", document.body).on("click", function () {
+        event.stopPropagation();
+        var domElement = $(this).get(0);
+        if (domElement.innerHTML === "▷") {
+            domElement.innerHTML = "◢";
+        }
+        else if (domElement.innerHTML === "◢") {
+            domElement.innerHTML = "▷";
+        }
+    });
+}); 
