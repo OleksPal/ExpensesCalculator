2e0d19e13d90d3794c4da24e6027f7b2556b2b41
User <oleksandr.palamarchuk1310@gmail.com>
2024-07-18 12:34:40 +0300

    Fix the ShowChecks view design template

--- wwwroot/js/managers/check-items-manager.js
+++ wwwroot/js/managers/check-items-manager.js
@@ -2,7 +2,6 @@ function getCheckItemsManager(checkId, dayId) {
     $(document).ready(function () {
         if ($(`#check-${checkId}-Items`).is(':empty')) {
             $.get(`/Checks/GetCheckItemsManager/${checkId}?dayExpensesId=${dayId}`, function (data) {
-                $(`#checkDropper-${checkId}`).html('&#9698;');
                 $(`#check-${checkId}-Items`).html(data);
                 $(`#check-${checkId}`).collapse('show');
             });
@@ -10,7 +9,6 @@ function getCheckItemsManager(checkId, dayId) {
         else {
             var collapsedRow = document.getElementById(`check-${checkId}`);
             $(`check-${checkId}`).collapse('hide');
-            $(`#checkDropper-${checkId}`).html('&#9655;');
 
             collapsedRow.addEventListener('hidden.bs.collapse', () => {
                 $(`#check-${checkId}-Items`).empty();
