e250749cd0f0bfee9645f9d3dc7f018cd032eff1
User <oleksandr.palamarchuk1310@gmail.com>
2024-08-12 10:38:11 +0300

    Fix GetById test methods

--- ExpensesCalculator.Tests/UnitTests/DayExpensesServiceUnitTests.cs
+++ ExpensesCalculator.Tests/UnitTests/DayExpensesServiceUnitTests.cs
@@ -13,14 +13,13 @@ namespace ExpensesCalculator.UnitTests
                 ?? throw new ArgumentNullException(nameof(IDayExpensesService));
         }
 
+        #region GetAllDays method
         [Fact]
         public async void GetAllDaysForDefaultUser()
         {
-            var emptyDayCollection = new List<DayExpenses>();
-
             var dayCollection = await _dayExpensesService.GetAllDays();
 
-            Assert.Equal(emptyDayCollection, dayCollection);
+            Assert.Single(dayCollection);
         }
 
         [Fact]
@@ -33,7 +32,9 @@ namespace ExpensesCalculator.UnitTests
 
             Assert.Equal(emptyDayCollection, dayCollection);
         }
+        #endregion
 
+        #region GetDayExpensesById
         [Fact]
         public async void GetDayExpensesByIdThatDoesNotExists()
         {
@@ -45,15 +46,19 @@ namespace ExpensesCalculator.UnitTests
         [Fact]
         public async void GetDayExpensesByIdThatExists()
         {
+            var dayExpensesDate = new DateOnly(2024, 1, 1);
+
             var dayExpenses = await _dayExpensesService.GetDayExpensesById(1);
 
-            Assert.NotNull(dayExpenses);
+            Assert.Equal(dayExpensesDate, dayExpenses.Date);
         }
+        #endregion
 
+        #region GetDayExpensesByIdWithChecks method
         [Fact]
         public async void GetDayExpensesByIdWithChecksThatDoesNotExists()
         {
-            var dayExpenses = await _dayExpensesService.GetDayExpensesById(5);
+            var dayExpenses = await _dayExpensesService.GetDayExpensesByIdWithChecks(5);
 
             Assert.Null(dayExpenses);
         }
@@ -61,15 +66,19 @@ namespace ExpensesCalculator.UnitTests
         [Fact]
         public async void GetDayExpensesByIdWithChecksThatExists()
         {
-            var dayExpenses = await _dayExpensesService.GetDayExpensesById(1);
+            var dayExpensesDate = new DateOnly(2024, 1, 1);
 
-            Assert.NotNull(dayExpenses);
+            var dayExpenses = await _dayExpensesService.GetDayExpensesByIdWithChecks(1);
+
+            Assert.Equal(dayExpensesDate, dayExpenses.Date);
         }
+        #endregion
 
+        #region GetFullDayExpensesById
         [Fact]
         public async void GetFullDayExpensesByIdThatDoesNotExists()
         {
-            var dayExpenses = await _dayExpensesService.GetDayExpensesById(5);
+            var dayExpenses = await _dayExpensesService.GetFullDayExpensesById(5);
 
             Assert.Null(dayExpenses);
         }
@@ -77,9 +86,12 @@ namespace ExpensesCalculator.UnitTests
         [Fact]
         public async void GetFullDayExpensesByIdThatExists()
         {
-            var dayExpenses = await _dayExpensesService.GetDayExpensesById(1);
+            var dayExpensesDate = new DateOnly(2024, 1, 1);
 
-            Assert.NotNull(dayExpenses);
+            var dayExpenses = await _dayExpensesService.GetFullDayExpensesById(1);
+
+            Assert.Equal(dayExpensesDate, dayExpenses.Date);
         }
+        #endregion
     }
 }
