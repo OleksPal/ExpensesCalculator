bae4ce5893f298db0e037e8f462e69fbb23ba399
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-12 19:46:09 +0300

    Create a CreateDayExpensesRequestDto and a mapper for him

---
+++ src/Dtos/DayExpenses/CreateDayExpensesRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesCalculator.Dtos.DayExpenses
+{
+    public class CreateDayExpensesRequestDto
+    {
+        [Required(ErrorMessage = "Please enter expenses date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateOnly Date { get; set; }
+
+        [Display(Name = "Participant list")]
+        [Required(ErrorMessage = "Please enter participants list")]
+        public ICollection<string> ParticipantList { get; set; }
+    }
+}
---
+++ src/Mappers/DayExpensesMappers.cs
@@ -0,0 +1,22 @@
+﻿using ExpensesCalculator.Dtos.DayExpenses;
+using ExpensesCalculator.Models;
+
+namespace ExpensesCalculator.Mappers
+{
+    public static class DayExpensesMappers
+    {
+        public static DayExpenses ToDayExpenses(this CreateDayExpensesRequestDto dto, string creatorsUsername)
+        {
+            return new DayExpenses
+            {
+                Checks = new List<Check>(),
+                Date = dto.Date,
+                ParticipantsList = dto.ParticipantList,
+                PeopleWithAccessList = new List<string>
+                {
+                    creatorsUsername
+                }
+            };
+        }
+    }
+}
