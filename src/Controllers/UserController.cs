d52b9a56275c59a98fd6e49080fe1772d1c4e88e
User <oleksandr.palamarchuk1310@gmail.com>
2024-10-05 13:47:33 +0300

    Add a user controller

---
+++ src/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using ExpensesCalculator.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesCalculator.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            try
+            {
+                if (!TryValidateModel(registerDto))
+                    return BadRequest(ModelState);
+
+                var newUser = new User
+                {
+                    UserName = registerDto.UserName,
+                    Email = registerDto.Email
+                };
+
+                var createdUser = await _userManager.CreateAsync(newUser, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(newUser, "User");
+
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok(
+                            new NewUserDto
+                            {
+                                UserName = newUser.UserName,
+                                Email = newUser.Email
+                            }
+                        );
+                    }
+                    return StatusCode(500, roleResult.Errors);
+                }
+
+                return StatusCode(500, createdUser.Errors);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("loginWithUsername")]
+        public async Task<IActionResult> LoginWithUsername([FromBody] LoginWithUsernameDto loginDto)
+        {
+            if (!TryValidateModel(loginDto))
+                return BadRequest(ModelState);
+
+            var user = await GetUserByUsername(loginDto.UserName);
+
+            if (user is null)
+                return Unauthorized("Invalid username");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("UserName not found and/or password is wrong");
+            }
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = loginDto.UserName,
+                    Email = user.Email
+                }
+            );
+        }
+
+        protected virtual async Task<User> GetUserByUsername(string userName)
+        {
+            return await _userManager.Users.FirstOrDefaultAsync(user => user.UserName == userName.ToLower());
+        }
+    }
+}
