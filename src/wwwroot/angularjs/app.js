006992a37c60fb141dbfeeba65a946c504f1d65f
User <oleksandr.palamarchuk1310@gmail.com>
2024-10-15 20:29:10 +0300

    Add angularjs controller for DayExpenses views

--- src/wwwroot/angularjs/app.js
+++ src/wwwroot/angularjs/app.js
@@ -1,1 +1,13 @@
 ﻿var expensesCalculatorApp = angular.module('expensesCalculatorApp', []);
+
+expensesCalculatorApp.controller('DayExpensesCtrl', ['$scope', function ($scope) {
+    $http.get('/DayExpenses/GetAllDays')
+        .then(getAllDaysSuccessfulCallback, getAllDaysErrorCallback);
+
+    function getAllDaysSuccessfulCallback(response) {
+        $scope.days = response.data;
+    }
+    function getAllDaysErrorCallback(error) {
+        console.log(error);
+    }
+}])
