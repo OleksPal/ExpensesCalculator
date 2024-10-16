3a09e014a6ba884c01c4b9461bb7dcdf07db33c0
User <oleksandr.palamarchuk1310@gmail.com>
2024-10-16 14:32:45 +0300

    Fix buttons onclick event

--- src/wwwroot/angularjs/app.js
+++ src/wwwroot/angularjs/app.js
@@ -2,7 +2,6 @@
 
 expensesCalculatorApp.controller('DayExpensesCtrl', ['$scope', '$http', function ($scope, $http) {
     
-
     $http.get('/DayExpenses/GetAllDays')
         .then(getAllDaysSuccessfulCallback, getAllDaysErrorCallback);
 
@@ -12,4 +11,25 @@ expensesCalculatorApp.controller('DayExpensesCtrl', ['$scope', '$http', function
     function getAllDaysErrorCallback(error) {
         console.log(error);
     }
+
+    $scope.showModalForDayExpensesShare = function(dayId) {
+        $http.get('/DayExpenses/ShareDayExpenses/' + dayId).then(function (response) {
+            modalContent = angular.element(document.querySelector('#modal-content'));
+            modalContent.html(response.data);
+        });
+    };
+
+    $scope.showModalForDayExpensesEdit = function(dayId) {
+        $http.get('/DayExpenses/EditDayExpenses/' + dayId).then(function (response) {
+            modalContent = angular.element(document.querySelector('#modal-content'));
+            modalContent.html(response.data);
+        });
+    };
+
+    $scope.showModalForDayExpensesDelete = function(dayId) {
+        $http.get('/DayExpenses/DeleteDayExpenses/' + dayId).then(function (response) {
+            modalContent = angular.element(document.querySelector('#modal-content'));
+            modalContent.html(response.data);
+        });
+    };
 }])
