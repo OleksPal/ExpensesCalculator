2e8313ef38c43609e5ef379c73f4c12a7c71b359
User <oleksandr.palamarchuk1310@gmail.com>
2024-12-07 21:11:04 +0200

    Fix the tooltip switcher in the CalculateExpenses view

--- src/wwwroot/js/change-tooltip-visibility.js
+++ src/wwwroot/js/change-tooltip-visibility.js
@@ -1,24 +1,15 @@
 function changeTooltipVisibility() {
-    var leftTooltipstextElems = document.querySelectorAll('.left-tooltiptext');
-    var topTooltipstextElems = document.querySelectorAll('.top-tooltiptext');
+    var tooltipstextElems = document.querySelectorAll('.tooltiptext');
 
     if (document.getElementById('tooltipSwitcher').checked) {
         document.getElementById('tooltipSwitcherText').innerHTML = 'Disable tooltips';
-        leftTooltipstextElems.forEach(function (el) {
-            el.style.display = "block";
-        });
-
-        topTooltipstextElems.forEach(function (el) {
+        tooltipstextElems.forEach(function (el) {
             el.style.display = "block";
         });
     }
     else {
         document.getElementById('tooltipSwitcherText').innerHTML = 'Enable tooltips';
-        leftTooltipstextElems.forEach(function (el) {
-            el.style.display = "none";
-        });
-
-        topTooltipstextElems.forEach(function (el) {
+        tooltipstextElems.forEach(function (el) {
             el.style.display = "none";
         });
     }
