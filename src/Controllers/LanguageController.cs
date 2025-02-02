c63b9ed701edd91fe875705440120473bd9e7b54
User <oleksandr.palamarchuk1310@gmail.com>
2025-02-02 19:16:40 +0200

    Add the language controller

---
+++ src/Controllers/LanguageController.cs
@@ -0,0 +1,29 @@
+﻿using ExpensesCalculator.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExpensesCalculator.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            string? languageCookie = Request.Cookies["languageCulture"];
+
+            if (languageCookie != null)
+            {
+                ChangeLanguage(languageCookie);
+            }
+        }
+
+        public void ChangeLanguage(string language)
+        {
+            var languageManager = new LanguageManager();
+            languageManager.ChangeLanguageCulture(language);
+
+            Response.Cookies.Append("languageCulture", language);
+        }
+    }
+}
